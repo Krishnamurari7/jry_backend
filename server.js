791a7990ca04e94cb7bdc03d27fac4a27021cb6d
Krishna murari kumar <ayaankumar7488@gmail.com>
2025-04-13 14:05:54 +0530

    updated server.js file

--- server.js
+++ server.js
@@ -12,9 +12,15 @@ const connectionRoutes = require('./routes/connectionRoutes');
 const queriesRoutes = require('./routes/queriesRoutes');
 
 
+
 const PORT = process.env.PORT || 5000;
 const app = express();
 connectDB();
+
+const FRONTEND_URLS = [
+  "http://localhost:5000", // Localhost for development
+  "https://jry-backend.vercel.app/", // Deployed frontend
+];
 
 
 // Middleware
@@ -34,6 +40,21 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected:", process.env.MONGO_URI || "127.0.0.1"))
   .catch((err) => console.error("MongoDB connection error:", err));
+
+  app.use(
+    cors({
+      origin: function (origin, callback) {
+        if (!origin || FRONTEND_URLS.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("CORS Not Allowed"));
+        }
+      },
+      credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
 
 // Create uploads directory if it doesn't exist
 const uploadsDir = path.join(__dirname, "uploads");
